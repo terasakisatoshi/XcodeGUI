895265f377571207337ff45fafcaa73d042b5961
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2017-04-24 00:50:04 +0900

    first commit

---
+++ HelloSwift/HelloSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by 寺崎敏志 on 2016/08/28.
+//  Copyright © 2016年 寺崎敏志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
